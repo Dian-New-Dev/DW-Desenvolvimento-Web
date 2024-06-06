ce01a0ab47b76dd427902f7d294e7ca92ca1cc39
Diego <diannewdev@gmail.com>
2024-06-06 17:06:41 -0400

    mudei o mostruario de templates para iframe

---
+++ contentObject.js
@@ -0,0 +1,16 @@
+const sitesAdv = [
+    'https://dian-new-dev.github.io/LP-0/', 
+    'https://dian-new-dev.github.io/LP-3/', 
+    'https://dian-new-dev.github.io/LP-8/', 
+    'https://dian-new-dev.github.io/LP-6/', 
+    'https://dian-new-dev.github.io/LP-5/', 
+    'https://dian-new-dev.github.io/LP-2/', 
+    'https://dian-new-dev.github.io/LP-1/', 
+    'https://dian-new-dev.github.io/LP-4/',
+]
+
+const sitesTer = [
+    'https://dian-new-dev.github.io/LPT-3/',
+    'https://dian-new-dev.github.io/LPT-1/',
+    'https://dian-new-dev.github.io/LPT-2/',
+]
--- dobra4-portfolio.js
+++ dobra4-portfolio.js
@@ -1,63 +1,141 @@
-const dobra = document.querySelector('.dobra4-B');
-const images = dobra.querySelectorAll('img');
-images[0].style.display = 'block'
+// const dobra = document.querySelector('.dobra4-B');
+// const images = dobra.querySelectorAll('img');
+// images[0].style.display = 'block'
 
-const desktopButton = document.getElementById('desktopButton');
-const mobileButton = document.getElementById('mobileButton')
+// const desktopButton = document.getElementById('desktopButton');
+// const mobileButton = document.getElementById('mobileButton')
 
-desktopButton.addEventListener('click', function() {
-    mostrarImagensDesktop();
+// desktopButton.addEventListener('click', function() {
+//     mostrarImagensDesktop();
+// })
+
+// mobileButton.addEventListener('click', function() {
+//     mostrarImagensMobile();
+// })
+
+// const setaEsquerda = document.querySelector('.seta-esquerda');
+// const setaDireita = document.querySelector('.seta-direita');
+
+// setaEsquerda.addEventListener('click', function() {
+//     scrollLeft();
+// })
+
+// setaDireita.addEventListener('click', function() {
+//     scrollRight();
+// })
+
+// function scrollRight() {
+//     for (let i = 0; i < images.length - 1; i++) {
+//         const imageStyle = window.getComputedStyle(images[i]);
+//         if (imageStyle.getPropertyValue('display') === 'block' && images[i+1]) {
+//             images[i].style.display = 'none';
+//             images[i+1].style.display = 'block';
+//             return;
+//         } else {
+//             images[i].style.display = 'none';
+//         }
+//     }
+// }
+
+// function scrollLeft() {
+//     for (let i = 0; i < images.length; i++) {
+//         const imageStyle = window.getComputedStyle(images[i]);
+//         if (imageStyle.getPropertyValue('display') === 'block' && images[i] != images[0]) {
+//             images[i].style.display = 'none';
+//             images[i-1].style.display = 'block';
+//             return;
+//         } else {
+            
+//         }
+//     }
+// }
+
+// function mostrarImagensMobile() {
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].src = 'images/templates/template' + (i) + '-mobile.jpg';
+//     }
+// }
+
+// function mostrarImagensDesktop() {
+//     for (let i = 0; i < images.length; i++) {
+//         images[i].src = 'images/templates/blender-template' + (i) + '.png';
+//     }
+// }
+
+const sitesIframe = document.getElementById('sites-iframe');
+
+let siteCounter = 0; // para usar como index no array de sites
+
+let sinalizador = 0;
+    // 0 = advocacia;
+    // 1 = psicoterapia;
+
+
+document.addEventListener("DOMContentLoaded", function (){
+    mostrarSites('adv');
+});
+
+function mostrarSites(ramo) {
+    if (ramo === 'adv') {
+        siteCounter = 0;
+        sitesIframe.src = sitesAdv[siteCounter]
+        sinalizador = 0;
+        
+
+    } else if (ramo === 'ter') {
+        siteCounter = 0;
+        sitesIframe.src = sitesTer[siteCounter]
+        sinalizador = 1;
+        
+
+    }
+}
+
+const advButton = document.getElementById('advButton');
+const terButton = document.getElementById('terButton');
+
+advButton.addEventListener('click', function() {
+    mostrarSites('adv');
+    
 })
 
-mobileButton.addEventListener('click', function() {
-    mostrarImagensMobile();
+terButton.addEventListener('click', function() {
+    mostrarSites('ter')
 })
 
 const setaEsquerda = document.querySelector('.seta-esquerda');
 const setaDireita = document.querySelector('.seta-direita');
 
-setaEsquerda.addEventListener('click', function() {
-    scrollLeft();
+setaDireita.addEventListener('click', function () {
+    avançarDireita();
 })
 
-setaDireita.addEventListener('click', function() {
-    scrollRight();
+function avançarDireita() {
+    if (sinalizador === 0) { // para sites de advocacia
+        if (sitesAdv[(siteCounter+1)])
+            siteCounter ++;
+            sitesIframe.src = sitesAdv[siteCounter];
+    } else if (sinalizador === 1) { // para sites de terapia
+        if (sitesTer[(siteCounter+1)])
+            siteCounter ++;
+            sitesIframe.src = sitesTer[siteCounter];
+
+    } // adicionar novos else if para novos ramos
+}
+
+setaEsquerda.addEventListener('click', function () {
+    retornarEsquerda();
 })
 
-function scrollRight() {
-    for (let i = 0; i < images.length - 1; i++) {
-        const imageStyle = window.getComputedStyle(images[i]);
-        if (imageStyle.getPropertyValue('display') === 'block' && images[i+1]) {
-            images[i].style.display = 'none';
-            images[i+1].style.display = 'block';
-            return;
-        } else {
-            images[i].style.display = 'none';
-        }
-    }
-}
+function retornarEsquerda() {
+    if (sinalizador === 0) { // para sites de advocacia
+        if (sitesAdv[(siteCounter-1)])
+            siteCounter --;
+            sitesIframe.src = sitesAdv[siteCounter];
+    } else if (sinalizador === 1) { // para sites de terapia
+        if (sitesTer[(siteCounter-1)])
+            siteCounter --;
+            sitesIframe.src = sitesTer[siteCounter];
 
-function scrollLeft() {
-    for (let i = 0; i < images.length; i++) {
-        const imageStyle = window.getComputedStyle(images[i]);
-        if (imageStyle.getPropertyValue('display') === 'block' && images[i] != images[0]) {
-            images[i].style.display = 'none';
-            images[i-1].style.display = 'block';
-            return;
-        } else {
-            
-        }
-    }
-}
-
-function mostrarImagensMobile() {
-    for (let i = 0; i < images.length; i++) {
-        images[i].src = 'images/templates/template' + (i) + '-mobile.jpg';
-    }
-}
-
-function mostrarImagensDesktop() {
-    for (let i = 0; i < images.length; i++) {
-        images[i].src = 'images/templates/blender-template' + (i) + '.png';
-    }
+    } // adicionar novos else if para novos ramos
 }
